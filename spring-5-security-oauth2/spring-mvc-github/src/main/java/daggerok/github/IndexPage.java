86dc20236ddd3f1047b1a7c98aa9db7518d7a1e2
Maksim Kostromin <daggerok@gmail.com>
2018-07-13 00:11:37 +0300

    dirty hack, have a better idea ? (C) whatthecommit.com

--- spring-5-security-oauth2/spring-mvc-github/src/main/java/daggerok/github/IndexPage.java
+++ spring-5-security-oauth2/spring-mvc-github/src/main/java/daggerok/github/IndexPage.java
@@ -1,5 +1,6 @@
 package daggerok.github;
 
+import io.vavr.collection.HashMap;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.reactive.function.client.ClientRequest;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -22,6 +24,51 @@ import java.util.Map;
 public class IndexPage {
 
   final OAuth2AuthorizedClientService authorizedClientService;
+
+  @ResponseBody
+  @GetMapping("/api/v1/json-data")
+  public Map map(final OAuth2AuthenticationToken authentication) {
+
+    final OAuth2AuthorizedClient authorizedClient =
+        authorizedClientService.loadAuthorizedClient(
+            authentication.getAuthorizedClientRegistrationId(),
+            authentication.getName());
+
+    return HashMap.of(
+        "authorizedClient", HashMap.of(
+            "accessToken", HashMap.of(
+                "issuedAt", authorizedClient.getAccessToken().getIssuedAt(),
+                "expiresAt", authorizedClient.getAccessToken().getExpiresAt(),
+                "tokenType", authorizedClient.getAccessToken().getTokenType(),
+                "tokenValue", authorizedClient.getAccessToken().getTokenValue(),
+                "scopes", authorizedClient.getAccessToken().getScopes()
+            ),
+            "clientRegistration", HashMap.of(
+                "scopes", authorizedClient.getClientRegistration().getScopes(),
+                "registrationId", authorizedClient.getClientRegistration().getRegistrationId(),
+                "redirectUriTemplate", authorizedClient.getClientRegistration().getRedirectUriTemplate(),
+                "providerDetails", authorizedClient.getClientRegistration().getProviderDetails(),
+                "clientSecret", authorizedClient.getClientRegistration().getClientSecret(),
+                "clientName", authorizedClient.getClientRegistration().getClientName(),
+                "clientId", authorizedClient.getClientRegistration().getClientId(),
+                "clientAuthenticationMethod", authorizedClient.getClientRegistration().getClientAuthenticationMethod(),
+                "authorizationGrantType", authorizedClient.getClientRegistration().getAuthorizationGrantType()
+            ),
+            "principalName", authorizedClient.getPrincipalName()
+        ).toJavaMap(),
+        "authentication", HashMap.of(
+            "name", authentication.getName(),
+            "details", authentication.getDetails(),
+            "authorizedClientRegistrationId", authentication.getAuthorizedClientRegistrationId(),
+            "authorities", authentication.getAuthorities(),
+            "credentials", authentication.getCredentials(),
+            "principal", HashMap.of(
+                "attributes", authentication.getPrincipal().getAttributes(),
+                "authorities", authentication.getPrincipal().getAuthorities()
+            ).toJavaMap()
+        ).toJavaMap()
+    ).toJavaMap();
+  }
 
   @GetMapping("/")
   public String index(Model model, OAuth2AuthenticationToken authentication) {
